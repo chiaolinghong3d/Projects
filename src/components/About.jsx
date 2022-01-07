60458cadfab9699e4267b475facf125c2f0f2ef4
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-07 05:54:54 -0800

    Added an email icon that will trigger mailto on click instead of having a contact page.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
-
+import { RiMailSendFill } from 'react-icons/ri';
+  
 function About() {
   return (
     <div className='content-item-container'>
       <div className="intro">
         <h1>Chiaoling <span style={{color: '#2377C6'}}>Hong</span></h1>
-        <br />
-        <br />
-        <h3>I am a software engineer who is passionate <br/>about user-centered design.</h3>
-        <br />
-        <br />
+        <br /><br />
+        <h3>I am a software engineer who is passionate <br/>about <span style={{color: '#4A9DEB'}}>user-centered design</span>.</h3>
+        <br /><br />
         <h3>clean.<span style={{color: '#4A9DEB'}}>simple</span>.design.</h3>
       </div>
       <div className="social-media-list pt-5">
@@ -19,6 +18,9 @@ function About() {
         </a>
         <a href='https://www.linkedin.com/in/chiaolinghong/' target='_blank' rel='noopener noreferrer'>
           <BsLinkedin className='social-media-icon'/>
+        </a>
+        <a href='mailto:chiaolinghong3d@gmail.com'>
+          <RiMailSendFill className='social-media-icon'/>
         </a>
       </div>
     </div>
