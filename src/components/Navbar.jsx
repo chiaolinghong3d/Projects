8bf9b65023a870f85406b6786b332411a3ee36a9
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-07 00:11:57 -0800

    Created Navbar using bootstrap responsive classnames - dtnamically generates the Navbar list with NavbarItems based on the given data.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import NavbarItem from './NavbarItem';
+import navbarItems from '../navbarItems';
 
 function Navbar() {
   return (
-    <>
-      <div className="navbar">
-        <div className="navbar-container container">
-          <Link to='/' className="navbar-logo">
-            Projects
-          </Link>
-        </div>
+    <nav
+      className='navbar navbar-expand-lg navbar-dark bd-dark flex-lg-column align-items-center py-2 text-center sticky-top'
+      id='sideNav'
+    >
+      <div className="text-center p-3">
+        <a className='navbar-brand js-scroll-trigger' href='#page-top'>
+          <span className='d-block d-lg-none'>Chiaoling Hong</span>
+          <span className='d-none d-lg-block'>
+            <img
+              className='img-fluid img-profile rounded-circle mx-auto mb-2 shadow'
+              src={`${process.env.PUBLIC_URL}/img/profile.jpg`}
+              alt='what I look like'
+            />
+          </span>
+        </a>
       </div>
-    </>
+      <button
+        className='navbar-toggler'
+        type='button'
+        data-bs-toggle='collapse'
+        data-bs-target='#navbarResponsive'
+        aria-controls='navbarResponsive'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon'></span>
+      </button>
+      <div className='collapse navbar-collapse' id='navbarResponsive'>
+        <ul className='navbar-nav flex-column w-100 justify-content-center'>
+          {navbarItems.map(({href, name}) => 
+            <NavbarItem href={href} name={name}/>
+          )}
+        </ul>
+      </div>
+    </nav>
   );
 }
 
---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavbarItem({ href, name }) {
+  return (
+    <li className='nav-item'>
+      <a className='nav-link js-scroll-trigger' href={`#${href}`}>
+        {name}
+      </a> 
+    </li>
+  );
+}
+
+export default NavbarItem;
