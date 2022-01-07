a2ced022e9789272805b22dbe671ad8be92371f5
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-07 00:13:14 -0800

    Create the main App.jsx file with some container divs and nests Navbar and Content inside.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import Content from "./components/Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className='container-fluid'>
+      <div className='row min-vh-100 flex-column flex-lg-row'>
+        <aside
+          className='col-lg-3 bg-dark'
+        >
+          <Navbar />
+        </aside>
+        <main className='col px-0'>
+          <Content />
+        </main>
+      </div>
     </div>
   );
 }
