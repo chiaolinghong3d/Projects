667efedcf76eb94a0994acf9fac8564e83fe692d
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-07 00:08:53 -0800

    Added a navbar item list so it now dynamically render the items.

---
+++ src/navbarItems.js
@@ -0,0 +1,24 @@
+const navbarItems = [
+  {
+    name: 'About',
+    href: 'about',
+  },
+  {
+    name: 'Skills',
+    href: 'skills',
+  },
+  {
+    name: 'Projects',
+    href: 'projects',
+  },
+  {
+    name: 'Education',
+    href: 'education',
+  },
+  {
+    name: 'Contact',
+    href: 'contact',
+  },
+]
+
+export default navbarItems;
