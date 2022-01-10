96e989737e1c457dc4b7135417ecae32b69366fe
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-10 04:41:12 -0800

    Refactored the code for the new data structure/

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React from "react";
+import ProjectLinks from "./ProjectLinks";
 
-function ProjectItem({name, description, images}) {
+function ProjectItem({
+  name,
+  description,
+  techstack,
+  github_url,
+  site_url,
+  components,
+}) {
   return (
     <div className='project-item'>
       <h2>{name}</h2>
-      <p>
-        {description}
-      </p>
-      <div className='project-gallery'>
-        {images.map(url => <img src={url} alt={`demo from ${name}`} className='img-project' />)}
-      </div>
+      <p>{description}</p>
+      <ProjectLinks github={github_url} site={site_url} />
+      <h3>Tech Stack</h3>
+      <p>{techstack}</p>
+      {components.map((component) => {
+        return (
+          <div className='project-item-border'>
+            <img
+              src={component.image_url}
+              alt={`demo of ${component.name}`}
+              className='img-project'
+            />
+            <h3>{component.name}</h3>
+            <ul>
+              {component.bullet_points.map((bp) => {
+                return (
+                  <li>
+                    <h4>{bp}</h4>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 }
