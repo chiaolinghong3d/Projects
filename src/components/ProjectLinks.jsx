66f178ea7d52b0bd5cc23daadd9e2fe2e2905239
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-10 14:14:17 -0800

    Added flex box for project links and adding bootstrap col so on phone it will change to rows.

--- src/components/ProjectLinks.jsx
+++ src/components/ProjectLinks.jsx
@@ -3,15 +3,19 @@ import { BsGithub, BsGlobe2 } from "react-icons/bs";
 
 function ProjectLinks({ github, site }) {
   return (
-    <div className='project-links-container'>
-      <a href={github}>
-        <BsGithub />
-        <h4>Github Repository</h4>
-      </a>
-      <a href={site}>
-        <BsGlobe2 />
-        <h4>Visit Site</h4>
-      </a>
+    <div className='row project-links-container'>
+      <div className='col-sm-6 col-md-4 d-flex align-items-center'>
+        <BsGithub className='mx-2'/>
+        <a href={github} className='blue'>
+          Github Repository
+        </a>
+      </div>
+      <div className='col-sm-3 col-md-4 d-flex align-items-center'>
+        <BsGlobe2 className='mx-2'/>
+        <a href={site} className='blue'>
+          Visit Site
+        </a>
+      </div>
     </div>
   );
 }
