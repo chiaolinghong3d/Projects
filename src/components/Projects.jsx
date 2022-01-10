0d9e56da1ecb560742f29f90acfcf8759eaa0819
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-10 04:40:39 -0800

    Added additional props from the new data structure.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import ProjectItem from './ProjectItem';
-import projectItems from '../projectItems.js';
+import React from "react";
+import ProjectItem from "./ProjectItem";
+import projectItems from "../projectItems.js";
 
 function Projects() {
   return (
-    <div className='content-item-container project-item' id="projects">
-      <h2 className='text-uppercase'>projects</h2>
-      {projectItems.map(({name, description, image_urls}) => <ProjectItem name={name} description={description} images={image_urls}/>)}
+    <div className='content-item-container project-item' id='projects'>
+      <h2 className='text-uppercase pb-3'>projects</h2>
+      <div className='content-item-border'>
+        {projectItems.map(({ name, description, techstack, components, github_url, site_url }) => (
+          <ProjectItem
+            name={name}
+            description={description}
+            techstack={techstack}
+            components={components}
+            github_url={github_url}
+            site_url={site_url}
+          />
+        ))}
+      </div>
     </div>
   );
 }
