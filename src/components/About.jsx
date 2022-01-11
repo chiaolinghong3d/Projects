11d1dc67738084bd1b5ce974666e8bbbc2b682e2
Chiaoling Hong <chiaolinghong3d@gmail.com>
2022-01-10 16:54:19 -0800

    Added padding in About section.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
     <div className='content-item-container' id='about'>
       <h2 className='text-uppercase pb-3'>about me</h2>
       <div className='content-item-border'>
-        <p>
+        <p className='pt-3'>
           I have a degree in 3D Computer Graphics and have worked on AR
           applications for almost 3 years as a 3D Artist. As both a designer and
           a web developer, I aim to combine both skills and build optimized user
